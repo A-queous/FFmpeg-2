458b2b281ba8e6fbb32960e6f2e653da62caa856
Brandon Sanchez <brandon.sanchez@ai.moda>
2022-03-11 13:34:38 -0500

    updated bash script for filter-branch

--- check_if_commit_not_exist.sh
+++ check_if_commit_not_exist.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-output = $(git branch -a --list "$1" --contains $2 | wc -c)
+OUTPUT=$(git branch -a --list "origin/master" --contains 8c58d6650f231f8e85c367acc67fc54da4e939c7 | wc -c)
+ZERO=0
 
-if $output -gt 0
-    return false
+if [ $OUTPUT -gt $ZERO ]
+then
+    echo exit 0
 else
-    return true
+    echo exit 1
 fi
