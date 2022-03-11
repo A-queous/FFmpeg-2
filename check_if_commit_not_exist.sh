70a8f4f143416ed1695ebb07c00c4241df844740
Brandon Sanchez <brandon.sanchez@ai.moda>
2022-03-11 13:35:52 -0500

    updated bash script

--- check_if_commit_not_exist.sh
+++ check_if_commit_not_exist.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-OUTPUT=$(git branch -a --list "origin/master" --contains 8c58d6650f231f8e85c367acc67fc54da4e939c7 | wc -c)
+OUTPUT=$(git branch -a --list "$1" --contains $2 | wc -c)
 ZERO=0
 
 if [ $OUTPUT -gt $ZERO ]
 then
-    echo exit 0
+    exit 0
 else
-    echo exit 1
+    exit 1
 fi
